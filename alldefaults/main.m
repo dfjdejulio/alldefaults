d0a79550fcb106592ba5d66f39176bcdf5dad444
Doug DeJulio <ddj@aisb.org>
2016-05-11 13:46:03 -0400

    Initial Commit

---
+++ alldefaults/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  alldefaults
+//
+//  Created by Doug DeJulio on 2016-05-11.
+//  Copyright © 2016 Doug DeJulio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
