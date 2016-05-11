d40e357734ac088421ebd7664acb74d5a0071641
Doug DeJulio <ddj@aisb.org>
2016-05-11 14:27:58 -0400

    First working implementation.

--- alldefaults/main.m
+++ alldefaults/main.m
@@ -10,8 +10,19 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        NSUserDefaults *shared = nil;
+        if (argc > 1) {
+            shared = [[NSUserDefaults alloc] initWithSuiteName:@(argv[1])];
+        }
+        NSDictionary *some = [defaults dictionaryRepresentation];
+        NSMutableDictionary *all = [[NSMutableDictionary alloc] initWithDictionary: some];
+        if (shared) {
+            NSDictionary *more = [shared dictionaryRepresentation];
+            [all addEntriesFromDictionary: more];
+        }
+        NSString *output = [NSString stringWithFormat:@"All defaults:\n\n%@\n", all];
+        puts([output UTF8String]);
     }
     return 0;
 }
